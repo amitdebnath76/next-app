ad85a568e38e8bdd73a1c0242e772cdbf99a8a3b
amitdebnath76 <debnath4455@gmail.com>
2024-12-03 19:30:05 +0600

    creating next js application name nest-app

---
+++ app/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navLink = [
+        {id:1,title:'DashBoard',url:'/'},
+        {id:2,title:'About',url:'/about'},
+        {id:3,title:'Portfolio',url:'/about'},
+        {id:4,title:'About',url:'/about'},
+        {id:5,title:'About',url:'/about'},
+    ]
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
