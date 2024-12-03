9cb2dc6e1f63bf3758d0e3a4e32a91bdaf9b449d
amitdebnath76 <debnath4455@gmail.com>
2024-12-03 23:35:31 +0600

    Added Navber And Footer in our project

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-between px-20 bg-slate-300 py-12'>
+        <div>@2024. Next APP. All rights reserved</div>
+        <div className='flex justify-around gap-7'>
+            <Image src='/1.png' alt='' height={20} width={20}></Image>
+            <Image src='/2.png' alt='' height={20} width={20}></Image>
+            <Image src='/3.png' alt='' height={20} width={20}></Image>
+            <Image src='/4.png' alt='' height={20} width={20}></Image>
+        </div>
+    </div>
   )
 }
 
--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 
@@ -11,10 +12,14 @@ const Navbar = () => {
         {id:6,title:'Dashboard',url:'/dashboard'},
     ]
   return (
-    <div>Navbar
+    <div className='flex justify-between container px-28 mt-16 '>
+        <div className='text-4xl font-bold bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 text-transparent bg-clip-text'><p>Next APP</p></div>
+        <div className='flex justify-center gap-6'>
         {
-            navLink.map((link,id)=> <Link key={link.id} href={link.url}>{link.title}</Link>)
+            navLink.map((link,id)=> <Link className= 'text-xl font-semibold text-green-400 hover:text-green-800' key={link.id} href={link.url}>{link.title}</Link>)
         }
+        </div>
+        <div><button className='bg-cyan-500 hover:bg-cyan-600 text-white font-blod text-xl px-3 py-2  rounded-md border-blue-700' onClick={()=>console.log('Log Out')}>Log Out</button></div>
     </div>
   )
 }
