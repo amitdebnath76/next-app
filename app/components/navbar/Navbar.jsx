d20ef5ddc8c65326af5040eebe7662dddda2bf3b
amitdebnath76 <debnath4455@gmail.com>
2024-12-03 19:43:56 +0600

    Added Navber to our project

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,15 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
     const navLink = [
-        {id:1,title:'DashBoard',url:'/'},
+        {id:1,title:'Home',url:'/'},
         {id:2,title:'About',url:'/about'},
-        {id:3,title:'Portfolio',url:'/about'},
-        {id:4,title:'About',url:'/about'},
-        {id:5,title:'About',url:'/about'},
+        {id:3,title:'Portfolio',url:'/portfolio'},
+        {id:4,title:'Blog',url:'/blog'},
+        {id:5,title:'Contact',url:'/contact'},
+        {id:6,title:'Dashboard',url:'/dashboard'},
     ]
   return (
-    <div>Navbar</div>
+    <div>Navbar
+        {
+            navLink.map((link,id)=> <Link key={link.id} href={link.url}>{link.title}</Link>)
+        }
+    </div>
   )
 }
 
